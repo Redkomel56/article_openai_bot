99f6bc01793fcd884bc700214be65f1467cc7847
Redkomel56 <75861573+Redkomel56@users.noreply.github.com>
2022-07-24 16:26:42 +0300

    Template for tutorial
    1.0

---
+++ handlers/users/help.py
@@ -0,0 +1,15 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandHelp
+
+from loader import dp
+
+
+@dp.message_handler(CommandHelp())
+async def bot_help(message: types.Message):
+    text = ("Список команд: ",
+            "/start - Начать диалог",
+            "/help - Получить справку",
+            "/audio - это команда для скачивания видео и отправка вам аудио сообщением")
+
+    await message.answer("\n".join(text))
+
---
+++ states/__init__.py
@@ -0,0 +1,1 @@
+from .dowload import Dowload
